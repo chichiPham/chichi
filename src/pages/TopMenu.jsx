891018811e0c912af9686a1c894263430e530874
Tung <ductungsky12@gmail.com>
2022-10-24 22:54:00 +0700

    changed logo

--- src/pages/TopMenu.jsx
+++ src/pages/TopMenu.jsx
@@ -1,13 +1,17 @@
 import { Button } from "@mui/material";
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import logo from "../assets/images/logoHIKKOSHI.png"
 
 const TopMenu = () => {
   return (
     <>
     
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <h5 className="my-0 mr-md-auto font-weight-normal">Relocation service</h5>
+      <h5 className="my-0 mr-md-auto font-weight-normal">
+        <a href="/"><img style={{width: '30%', cursor: 'pointer', marginLeft: '100px'}}  src={logo} alt="Relocation service" /></a>
+      </h5>
+      
       <nav className="my-2 my-md-0 mr-md-3">
 
         <NavLink to={"/"}>
